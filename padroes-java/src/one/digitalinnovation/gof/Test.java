f6489856f6ba5b0cf7b1937479b57282b08321b6
pedrograma <pedro.phfg11@gmail.com>
2024-07-11 19:21:01 -0300

    projeto padroes projeto

---
+++ padroes-java/src/one/digitalinnovation/gof/Test.java
@@ -0,0 +1,49 @@
+package one.digitalinnovation.gof;
+
+import one.digitalinnovation.gof.facede.Facede;
+import one.digitalinnovation.gof.singleton.SingletonEager;
+import one.digitalinnovation.gof.singleton.SingletonLazy;
+import one.digitalinnovation.gof.singleton.SingletonLazyHolder;
+import one.digitalinnovation.gof.strategy.*;
+
+public class Test {
+
+    public static void main(String[] args) {
+        //Singleton
+        //Testes Relacionado ao Design Pattern Singleton
+        SingletonLazy lazy = SingletonLazy.getInstancia();
+        System.out.println(lazy);
+        lazy = SingletonLazy.getInstancia();
+        System.out.println(lazy);
+        System.out.println("===");
+        SingletonEager eager = SingletonEager.getInstancia();
+        System.out.println(eager);
+        eager = SingletonEager.getInstancia();
+        System.out.println(eager);
+        System.out.println("===");
+        SingletonLazyHolder lazyHolder = SingletonLazyHolder.getInstancia();
+        System.out.println(lazyHolder);
+        lazyHolder = SingletonLazyHolder.getInstancia();
+        System.out.println(lazyHolder);
+        //Strategy
+        Comportamento normal = new ComportamentoNormal();
+        Comportamento defensivo = new ComportamentoDefensivo();
+        Comportamento agressivo = new ComportamentoAgressivo();
+
+        Robo robo = new Robo();
+        robo.setComportamento(normal);
+        robo.mover();
+        robo.mover();
+        robo.setComportamento(defensivo);
+        robo.mover();
+        robo.setComportamento(agressivo);
+        robo.mover();
+        robo.mover();
+
+        //Facede
+
+        Facede facede = new Facede();
+        facede.migrarCliente("Pedro","31646-47");
+    }
+
+}
---
+++ padroes-java/src/subsistema2/cep/CepApi.java
@@ -0,0 +1,19 @@
+package subsistema2.cep;
+
+public class CepApi {
+    private static CepApi instancia = new CepApi();
+
+    private CepApi() {
+        super();
+    }
+    public static CepApi getInstancia() {
+        return instancia;
+    }
+
+    public static String recuperarCidade(String cep) {
+        return "Belo Horizonte";
+    }
+    public static String recuperarEstado(String cep) {
+        return "MG";
+    }
+}
